f09af69816c6ff7e3cd175d614c20d6cd53d244c
Brandon <LQN1992@163.com>
2019-09-24 21:57:24 +0800

    print string

---
+++ test001.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   test.py
+@Time    :   2019/09/24 21:36:06
+@Author  :   lqnan 
+@Version :   1.0
+@Contact :   lqnan@outlook.com
+@License :   (C)Copyright 2019-2020, lqnan
+@Desc    :   None
+'''
+
+# here put the import lib
+for i in range(5):
+    print("hello Brandon")
